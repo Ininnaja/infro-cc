718d9275954afc3d014a0015bf71ec04f7a08201
Ininnaja <inthirajail94@gmail.com>
2024-12-12 15:49:34 +0700

    Edit some code

--- src/Page1.js
+++ src/Page1.js
@@ -7,7 +7,6 @@ import Buttonn from './component/Buttonn';
 import Footer from './component/Footer';
 
 
-
 function Page1() {
   return (
     <div className="App">
@@ -18,7 +17,7 @@ function Page1() {
         <Content 
         mainHeading="พี่ๆน้องๆคนไหนสนใจเกี่ยวกับเกม Esport รบกวนทำแบบสอบถามของทางชุมนุมให้หน่อยนะครับ แล้วพบกันเร็วๆนี้ รอติดตามได้เลยค้าบบ"
         subHeading1="เงื่อนไขเพิ่มเติม"
-        subHeading2="1) จัดหา Team นักเเข่ง 5-6 คน"
+        subHeading2="1) จัดหา Team นักเเข่ง 5-7 คน"
         subHeading3="2) มีเงินรางวัลให้กับผู้ชนะมูลค่ารวม 3000 บาท"
         subHeading4="3) เข้ารวมฟรี"
         subHeading5="4) สามารถนำอุปกรณ์ส่วนตัวเข้ามาใช้รวมการเเข่งได้"
